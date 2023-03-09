0bb32d4f7f1a660da9a9e55848a1c4366331d3d2
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-03-09 23:18:19 +0800

    First commit

---
+++ pack/admin.py
@@ -0,0 +1,58 @@
+from django.contrib import admin
+from .models import (
+    Pack,
+    GachaResource,
+    DevelopResource,
+    OtherItem,
+    GachaList,
+    DevelopList,
+    OtherList,
+)
+
+
+@admin.register(GachaResource)
+class GachaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DevelopResource)
+class DevelopAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OtherItem)
+class OtherAdmin(admin.ModelAdmin):
+    pass
+
+
+class GachaNestedInline(admin.TabularInline):
+    model = GachaResource
+    extra = 0
+
+
+class GachaInline(admin.TabularInline):
+    model = GachaList
+    extra = 0
+
+
+class DevelopInline(admin.TabularInline):
+    model = DevelopList
+    extra = 0
+
+
+class OtherInline(admin.StackedInline):
+    model = OtherList
+    extra = 0
+
+
+@admin.register(Pack)
+class PackAdmin(admin.ModelAdmin):
+    fields = (
+        ("name", "pack_id"),
+        "limitation",
+        ("price", "on_sale"),
+        "originium",
+        "note",
+    )
+    radio_fields = {"limitation": admin.HORIZONTAL}
+    inlines = [GachaInline, DevelopInline, OtherInline]
---
+++ pack/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+
+class GachaResource(models.Model):
+    name = models.CharField(max_length=40, help_text="名称")
+    orundum = models.IntegerField(help_text="等价合成玉")
+
+    def __str__(self):
+        return self.name
+
+
+class DevelopResource(models.Model):
+    name = models.CharField(max_length=40, help_text="名称")
+
+    def __str__(self):
+        return self.name
+
+
+class OtherItem(models.Model):
+    name = models.CharField(max_length=40, help_text="名称")
+    originium = models.IntegerField(blank=True, help_text="等价源石（不填表示难以估计）")
+
+    def __str__(self):
+        return self.name
+
+
+class Pack(models.Model):
+    name = models.CharField(max_length=40, help_text="名称")
+    pack_id = models.IntegerField(help_text="礼包ID")
+    limitation = models.CharField(
+        max_length=1,
+        choices=[
+            ("1", "once"),
+            ("m", "monthly"),
+            ("w", "weekly"),
+            ("y", "year"),
+            ("p", "permanent"),
+            ("l", "limited"),
+        ],
+        help_text="购买限制",
+    )
+    price = models.IntegerField(help_text="价格（元）")
+    on_sale = models.BooleanField(help_text="是否在售")
+    originium = models.IntegerField(help_text="源石")
+    gacha_resources = models.ManyToManyField(
+        GachaResource, help_text="抽卡资源", through="GachaList"
+    )
+    develop_resources = models.ManyToManyField(
+        DevelopResource, help_text="养成资源", through="DevelopList"
+    )
+    other_items = models.ManyToManyField(OtherItem, help_text="其它", through="OtherList")
+    note = models.TextField("备注", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class GachaList(models.Model):
+    gacha_resource = models.ForeignKey(GachaResource, on_delete=models.CASCADE)
+    pack = models.ForeignKey(Pack, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+
+class DevelopList(models.Model):
+    develop_resource = models.ForeignKey(DevelopResource, on_delete=models.CASCADE)
+    pack = models.ForeignKey(Pack, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+
+class OtherList(models.Model):
+    other_item = models.ForeignKey(OtherItem, on_delete=models.CASCADE)
+    pack = models.ForeignKey(Pack, on_delete=models.CASCADE)
+    count = models.IntegerField()
