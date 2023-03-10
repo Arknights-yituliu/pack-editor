de187c147565d31aef065b81a29b1d9dd8cb4cea
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-03-10 11:44:13 +0800

    Use enum for choices

--- pack/models.py
+++ pack/models.py
@@ -25,30 +25,43 @@ class OtherItem(models.Model):
 
 
 class Pack(models.Model):
-    name = models.CharField(max_length=40, unique=True, help_text="名称")
+    class Limitation(models.TextChoices):
+        ONCE = "1", "once"
+        MONTHLY = "m", "monthly"
+        WEEKLY = "w", "weekly"
+        YEAR = "y", "year"
+        PERMANENT = "p", "permanent"
+        LIMITED = "l", "limited"
+
+    name = models.CharField(
+        max_length=40,
+        unique=True,
+        help_text="名称",
+    )
     pack_id = models.IntegerField(unique=True, help_text="礼包ID")
     limitation = models.CharField(
         max_length=1,
-        choices=[
-            ("1", "once"),
-            ("m", "monthly"),
-            ("w", "weekly"),
-            ("y", "year"),
-            ("p", "permanent"),
-            ("l", "limited"),
-        ],
+        choices=Limitation.choices,
         help_text="购买限制",
     )
     price = models.IntegerField(help_text="价格（元）")
     on_sale = models.BooleanField(default=True, help_text="是否在售")
     originium = models.IntegerField(default=0, help_text="源石")
     gacha_resources = models.ManyToManyField(
-        GachaResource, help_text="抽卡资源", through="GachaList"
+        GachaResource,
+        help_text="抽卡资源",
+        through="GachaList",
     )
     develop_resources = models.ManyToManyField(
-        DevelopResource, help_text="养成资源", through="DevelopList"
+        DevelopResource,
+        help_text="养成资源",
+        through="DevelopList",
     )
-    other_items = models.ManyToManyField(OtherItem, help_text="其它", through="OtherList")
+    other_items = models.ManyToManyField(
+        OtherItem,
+        help_text="其它",
+        through="OtherList",
+    )
     note = models.TextField("备注", blank=True)
 
     def __str__(self):
