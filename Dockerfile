54b9441f1c0e672bb3787656169ed38dcbdb50aa
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-07-24 23:11:52 +0800

    更改时区；便于本地调试；添加PyPI镜像

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM debian:bullseye
 RUN sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 RUN apt update && apt upgrade -y
 RUN apt install -y python3-pip
+RUN pip config set global.index-url https://mirror.sjtu.edu.cn/pypi/web/simple
 
 WORKDIR /app
 
--- pack_editor/settings.py
+++ pack_editor/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["yituliu.site", "backend.yituliu.site"]
+ALLOWED_HOSTS = ["yituliu.site", "backend.yituliu.site", "127.0.0.1"]
 
 
 # Application definition
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
 
